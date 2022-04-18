8021f7f8cb7e3547a67b8550781b7582a624e673
kozincov <kozincov1998@gmail.com>
2022-04-18 18:29:33 +0300

    update navigation & create nav-menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import style from './Nav.module.css'
+import React, {useState} from 'react';
+import style from './Nav.module.scss'
+import styleApp from './../../App.module.scss'
+import {HiMenuAlt4, HiX} from 'react-icons/hi';
+import {motion} from 'framer-motion'
+
 
 export const Nav = () => {
+
+    const [toggle, setToggle] = useState(false)
+
     return (
-                <div className={style.nav}>
-                    <a href="">header</a>
-                    <a href="">about</a>
-                    <a href="">skills</a>
-                    <a href="">contact</a>
-                </div>
+        <nav className={style.app__nav}>
+            <ul className={style.app__nav_links}>
+                {['home', 'about', 'skills', 'projects', 'contact'].map((item) => (
+                    <li className={`${styleApp.app__flex} ${styleApp.p_text}`} key={`link-${item}`}>
+                        <div/>
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
+            </ul>
+            <div className={style.app__nav_menu}>
+                <HiMenuAlt4 onClick={() => setToggle(true)}/>
+
+                {toggle && (
+                    <motion.div whileInView={{x: [300, 0]}} transition={{duration: 0.85, ease: 'easeOut'}}>
+                        <HiX onClick={() => setToggle(false)}/>
+                        <ul>
+                            {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                                <li key={item}>
+                                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                                        {item}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </motion.div>
+                )}
+            </div>
+        </nav>
     );
 };
