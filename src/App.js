62952f80578f95670e255ac890c69622238a0fed
kozincov <kozincov1998@gmail.com>
2022-04-19 21:12:28 +0300

    refactoring code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import style from './App.module.scss'
 import {About, Footer, Header, Projects, Skills, Contact} from "./container";
+import {Nav} from "./components";
 
 export const App = () => {
     return (
         <div className={style.app}>
+            <Nav/>
             <Header/>
             <About/>
             <Skills/>
