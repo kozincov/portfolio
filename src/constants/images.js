2c511f34bfcc78652720a53dfee221dd92983100
kozincov <kozincov1998@gmail.com>
2022-04-19 21:14:15 +0300

    added png for header

---
+++ src/constants/images.js
@@ -0,0 +1,18 @@
+
+import profile3 from './../assets/profile3.png'
+import circle from './../assets/circle.svg'
+import flutter from './../assets/flutter.png'
+import redux from './../assets/redux.png'
+import sass from './../assets/sass.png'
+import bgIMGHeader from '../assets/bgIMGHeader.png'
+import react from '../assets/react.png'
+
+export default {
+    profile3,
+    circle,
+    flutter,
+    redux,
+    sass,
+    bgIMGHeader,
+    react,
+}
