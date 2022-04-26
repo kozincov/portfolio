11744cea9ae03b74cacde8621d1d82124af72ae5
kozincov <kozincov1998@gmail.com>
2022-04-26 18:52:57 +0300

    create component NavigationDots

---
+++ src/components/NavigationDots.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styleApp from './../App.module.scss'
+
+export const NavigationDots = ({active}) => {
+    return (
+        <div className={styleApp.app__navigation}>
+            {['home', 'about', 'work', 'skills', 'contact'].map((item, index) => (
+                <a
+                    href={`#${item}`}
+                    key={item + index}
+                    className={styleApp.app__navigation_dot}
+                    style={active === item ? {backgroundColor: '#313BAC'} : {}}
+                />
+            ))}
+        </div>
+    );
+};
+
