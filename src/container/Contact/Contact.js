a2807a41fb5cef3fcf53f9b65452466464267228
kozincov <kozincov1998@gmail.com>
2022-05-18 17:54:04 +0300

    update component Contact

--- src/container/Contact/Contact.js
+++ src/container/Contact/Contact.js
@@ -1,21 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import styleApp from './../../App.module.scss'
 import style from './Contact.module.scss'
-import {AppWrap} from "../../wrapper"
 
 const Contact = () => {
+    const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const { username, email, message } = formData;
+
+    const handleChangeInput = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = () => {
+        setLoading(true);
+
+        const contact = {
+            _type: 'contact',
+            name: formData.username,
+            email: formData.email,
+            message: formData.message,
+        };
+
+        // client.create(contact)
+        //     .then(() => {
+        //         setLoading(false);
+        //         setIsFormSubmitted(true);
+        //     })
+        //     .catch((err) => console.log(err));
+    };
+
     return (
-        <div className={style.contact}>
-            <div className={style.contactContainer}>
-                <h2 className={style.title}>Contact</h2>
-                <form className={style.form}>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
-                </form>
-                <button className={style.button}>Send</button>
+        <>
+            <h2 className={styleApp.head_text}>Take a coffee & chat with me</h2>
+
+            <div className={style.app__contact_cards}>
+                <div className={style.app__contact_card}>
+                    <img src={images.email} alt="email" />
+                    <a href="mailto:kozincov1998@gmail.com" className={styleApp.p_text}>kozincov1998@gmail.com</a>
+                </div>
+                <div className={style.app__contact_card}>
+                    <img src={images.mobile} alt="phone" />
+                    <a href="tel:+375 33 669 29 36" className={styleApp.p_text}>+375 (33) 669-29-36</a>
+                </div>
             </div>
-        </div>
+            {!isFormSubmitted ? (
+                <div className={`${style.app__contact_form} ${styleApp.app__flex}`}>
+                    <div className={styleApp.app__flex}>
+                        <input className={styleApp.p_text} type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
+                    </div>
+                    <div className={styleApp.app__flex}>
+                        <input className={styleApp.p_text} type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+                    </div>
+                    <div>
+            <textarea
+                className={styleApp.p_text}
+                placeholder="Your Message"
+                value={message}
+                name="message"
+                onChange={handleChangeInput}
+            />
+                    </div>
+                    <button type="button" className={styleApp.p_text} onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
+                </div>
+            ) : (
+                <div>
+                    <h3 className={styleApp.head_text}>
+                        Thank you for getting in touch!
+                    </h3>
+                </div>
+            )}
+        </>
     );
 };
 
-export default AppWrap(Contact, 'contact')
+export default AppWrap(
+    MotionWrap(Contact, style.app__contact),
+    'contact',
+    styleApp.app__primarybg,
+);
