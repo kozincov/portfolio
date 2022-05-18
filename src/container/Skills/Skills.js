58a39137929f8a3ff3dc3fb7f75c4f4eee5e256b
kozincov <kozincov1998@gmail.com>
2022-05-18 15:05:53 +0300

    update component Skills

--- src/container/Skills/Skills.js
+++ src/container/Skills/Skills.js
@@ -1,21 +1,79 @@
 import React from 'react';
 import style from './Skills.module.scss'
-import {Skill} from "./Skill/Skill";
-import {AppWrap} from "../../wrapper";
+import styleApp from './../../App.module.scss'
+import {AppWrap} from '../../wrapper';
+import ReactTooltip from 'react-tooltip'
+import {motion} from 'framer-motion';
 
 const Skills = () => {
+
+    const skills = [
+        {name: '', icon: '', }
+    ]
+    const experiences = []
+
     return (
-        <div className={style.skillsBlock}>
-            <div className={style.skillsContainer}>
-                <h2 className={style.title}>Skills</h2>
-                <div className={style.skills}>
-                    <Skill title={'JS'} description={'Lorem ipsum dolor sit amet.'}/>
-                    <Skill title={'React'} description={'Lorem ipsum dolor.'}/>
-                    <Skill title={'Redux'}
-                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. '}/>
+        <>
+            <h2 className={styleApp.head_text}>Skills & Experiences</h2>
+
+            <div className={style.app__skills_container}>
+                <motion.div className={style.app__skills_list}>
+                    {skills.map((skill) => (
+                        <motion.div
+                            whileInView={{ opacity: [0, 1] }}
+                            transition={{ duration: 0.5 }}
+                            className={`${style.app__skills_item} ${styleApp.app__flex}`}
+                            key={skill.name}
+                        >
+                            <div
+                                className={styleApp.app__flex}
+                                style={{ backgroundColor: skill.bgColor }}
+                            >
+                                <img src={skill.icon} alt={skill.name} />
+                            </div>
+                            <p className={styleApp.p_text}>{skill.name}</p>
+                        </motion.div>
+                    ))}
+                </motion.div>
+                <div className={style.app__skills_exp}>
+                    {experiences.map((experience) => (
+                        <motion.div
+                            className={style.app__skills_expItem}
+                            key={experience.year}
+                        >
+                            <div className={style.app__skills_expYear}>
+                                <p className={styleApp.bold_text}>{experience.year}</p>
+                            </div>
+                            <motion.div className={style.app__skills_expWorks}>
+                                {experience.works.map((work) => (
+                                    <>
+                                        <motion.div
+                                            whileInView={{ opacity: [0, 1] }}
+                                            transition={{ duration: 0.5 }}
+                                            className={style.app__skills_expWork}
+                                            data-tip
+                                            data-for={work.name}
+                                            key={work.name}
+                                        >
+                                            <h4 className={styleApp.bold_text}>{work.name}</h4>
+                                            <p className={styleApp.p_text}>{work.company}</p>
+                                        </motion.div>
+                                        <ReactTooltip
+                                            id={work.name}
+                                            effect="solid"
+                                            arrowColor="#fff"
+                                            className={style.skills_tooltip}
+                                        >
+                                            {work.desc}
+                                        </ReactTooltip>
+                                    </>
+                                ))}
+                            </motion.div>
+                        </motion.div>
+                    ))}
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
